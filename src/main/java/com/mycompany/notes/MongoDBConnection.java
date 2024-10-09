4ce1480148315558fc86711326d5805b6f1fd38e
FernandoIvan <fascenciocortes@gmail.com>
2024-10-09 17:09:52 -0600

    basic structure

--- src/main/java/com/mycompany/notes/MongoDBConnection.java
+++ src/main/java/com/mycompany/notes/MongoDBConnection.java
@@ -1,13 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.notes;
 
 /**
  *
  * @author Fernando Iván Ascencio Cortés
  */
-public class Notes {
+public class MongoDBConnection {
     
 }
